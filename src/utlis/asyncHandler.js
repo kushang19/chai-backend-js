d40d376af864c6d5e0a0fff9eafcd076333b103c
Kushang Vijay Gharat <kushang81@gmail.com>
2024-10-13 23:36:00 +0530

    utils code

---
+++ src/utlis/asyncHandler.js
@@ -0,0 +1,25 @@
+// Promise method
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch(err => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+// try catch method
+
+// const asyncHandler = (fnc) => async(error, req, res, next) => {
+//     try {
+//         await fnc(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
