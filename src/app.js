d23e1a3ffddfa14accbd00f81ed441b8e16fa5d1
Kushang Vijay Gharat <kushang81@gmail.com>
2024-10-15 00:29:35 +0530

    file upload

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
 
 export {app}
