d208424478e5a8bc5e3df92d69c619e74fd90fa4
Kushang Vijay Gharat <kushang81@gmail.com>
2024-10-22 01:44:57 +0530

    login and logout feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utlis/cloudinary.js";
 import { ApiResponse } from "../utlis/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        // saving into database
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false });
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // res.status(200).json({
     //     message: 'ok'
@@ -35,6 +52,8 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // check for images, check for avatar 
     const avatarLocalPath = req.files?.avatar[0]?.path;
+    console.log("Avatar ==> ", avatarLocalPath);
+    
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
@@ -79,4 +98,87 @@ const registerUser = asyncHandler(async (req, res) => {
     
 });
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check 
+    // access and refresh token
+    // send cookie
+
+    // req body -> data
+    const {email, username, password} = req.body;
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    });
+
+    if(!user){
+        throw new ApiError(404, "User does not exist");
+    };
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials");
+    };
+
+    const {accessToken, refreshToken} = generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, refreshToken, accessToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+});
+
+const logoutUser = asyncHandler(async(res,req) => {
+   await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200, {}, "User Logged Out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./env"
+    path: "./.env"
 })
 
 connectDB()
@@ -15,10 +15,7 @@ connectDB()
 .catch(err => {
     console.log("MONGO db connection failed !!! ", err );
     
-})
-
-
-
+});
 
 
 
