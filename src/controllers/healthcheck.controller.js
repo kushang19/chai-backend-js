3480dae7b8edccda13d6c51be39dc8d8084c3bec
kushang <kushang.gharat@quantique.ai>
2024-11-07 18:11:11 +0530

    get videos api

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,23 +3,13 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-  try {
+
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
 
-    const serviceIsHealthy = true; 
-    if (!serviceIsHealthy) {
-      console.log("ERROR:: XXXXXXXXXXXXXXXXXXX");
-    return  res
-        .status(503)
-        .json( new ApiResponse( 503, { status: "Service Unavailable" }, "Service health check failed" ) );
-    }
    return res
       .status(200)
-      .json( new ApiResponse(200, { status: "OK" }, "Service is running smoothly") )
-  } 
-  catch (error) {
-    throw new ApiError(500, error.message || "Internal Server Error");
-  }
+      .json( new ApiResponse(200, { status: "OK!!" }, "Service is running smoothly") )
+
 });
 
 export { healthcheck };
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,15 +1,105 @@
-import mongoose, {isValidObjectId} from "mongoose"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 import {Video} from "../models/video.model.js"
 import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import {ApiResponse} from "../utils/ApiResponse.js"
+import mongoose, {isValidObjectId} from "mongoose"
+import { Like } from "../models/like.model.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+
+    console.log(userId);
+    const pipeline = [];
+
+    if (query) {
+        pipeline.push({
+            $search: {
+                index: "search-videos",
+                text:{
+                    query: query,
+                    path: ["title", "description"]
+                }
+            }
+        });
+    }
+
+    if (userId) {
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid userId");
+        }
+
+        pipeline.push({
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        });
+    }
+
+    // fetch videos only that are set isPublished as true
+    pipeline.push({
+        $match: {
+            isPublished: true
+        }
+    });
+
+    //sortBy can be views, createdAt, duration
+    //sortType can be ascending(-1) or descending(1)
+
+    if(sortBy && sortType){
+        pipeline.push({
+            $sort: {
+                [sortBy] : sortType === "asc" ? 1 : -1
+            }
+        });
+    }else{
+        pipeline.push({
+            $sort: {
+                createdAt: -1
+            }
+        });
+    }
+
+    pipeline.push(
+        {
+            $lookup: {
+                from:"users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            uername: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$ownerDetails"
+        }
+
+    )
+
+    const videoAggregate = Video.aggregate(pipeline);
+
+    const options = {
+        page: parseInt(page,10),
+        limit: parseInt(limit, 10)
+    };
+
+    const video = await Video.aggregatePaginate(videoAggregate, options);
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Videos fetched successfully"))
+
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
